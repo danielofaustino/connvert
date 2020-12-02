5e1963fa7f570398bec682607fff58c237fa0f74
danielofaustino <danieloliveirafaustino@gmail.com>
2020-12-01 22:39:49 -0300

    Middlewares, uuid

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { uuid, isUuid } = require('uuidv4');
 
 const app = express();
 
@@ -20,46 +21,41 @@ app.use(express.json());
 const debts = [];
 
 
+//Middlewares
+
+function logRequests(req,res, next) {
+  const { method, url} = req;
+
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+  console.log(logLabel);
+  console.time(logLabel)
+
+  next( );
+
+  console.timeEnd(logLabel)
+}
+
+function validateDebtId(req,res, next){
+  const { id } = req.params;
+
+  if(!isUuid(id)){
+    return res.status(400).json({ error: 'Invalid Debt ID' });
+  }
+  return next();
+}
+
+app.use(logRequests);
+app.use('/debts/:id', validateDebtId);
+
 // routes: /debts
-
-
-
-
-
-
-
-//3 - get a specified debt
-app.get('/debts/:id', (req, res) =>{
-
-  let { id } = req.params;
-  console.log(id)
-  
-  let debt = debts.find(debt => debt.id_debt == id)
- 
-  
-  return res.json(debt);
-})
-
-
-//2 - get all debts related to a specified client
-app.get('/debts', (req, res) => {
-
-  const { client } = req.query;
-
-  
-  const results = client 
-    ? debts.filter(debt => debt.client.includes(client))
-    : debts;
-
-  return res.json(results);
-})
-
-
 
 
 // 1 Add a new debt
 app.post('/debts', (req, res) =>{
-  const debt = req.body;
+  const { client, reason, value, date } = req.body;
+
+  debt = {id_debt: uuid(), client, reason, value, date}
 
   debts.push(debt)
 
@@ -68,10 +64,42 @@ app.post('/debts', (req, res) =>{
 })
 
 
+//2 - get all debts related to a specified client
+app.get('/debts', (req, res) => {
+
+  const { client } = req.query;
+
+  if( client == 0) {
+    return res.status(400).json({ error: 'Client not found'})
+  }
+  
+  let results = debts.filter(x => x.client == client)
+
+  return res.json(results);
+
+})
+
+app.get('/debts/:id', (req, res) =>{
+  const { id } = req.params;
+
+  const debt = debts.find(x => x.id_debt == id)
+
+  return res.json(debt)
+})
 
 
-// Change some debt information
+
+
+
+//4 Change some debt information
 app.put('/debts/:id', (req, res) =>{
+  const { id } = req.params;
+  const debt = req.body;
+
+  const debtIndex = debts.findIndex(x => x.id_debt == id);
+  
+  debts[debtIndex] = debt;
+  return res.json(debt);
 
 })
 
