c6c8d5c4ea81c0f51937f02c83c725553a8437e6
danielofaustino <danieloliveirafaustino@gmail.com>
2020-12-03 00:05:15 -0300

    Creating routing to pages with react router dom

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.min.css";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import  { Home }  from "./pages/Home";
+import  { Debts }  from "./pages/Debts";
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/debts" component={Debts} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
---
+++ frontend/src/pages/Debts.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from '../components/NavBar';
+
+
+export const Debts =()=> {
+  return (
+    <>
+      <NavBar />
+      <div>Teste</div>
+    </>
+  );
+}
+
